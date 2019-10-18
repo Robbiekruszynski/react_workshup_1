78c4c8068a698d53b6fbd60ed65ae3e17e128e23
robbie <robbiekruszynski@gmail.com>
2019-10-18 11:01:09 -0700

    finish jsx work

--- jsx-workshop/src/index.js
+++ jsx-workshop/src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+function getButtonText() {
+    return 'Click on me';
+}
 const App = () => {
-  return <div> Hi there!</div>;
+
+    const buttonText = 'Click me ';
+
+  return ( 
+  <div> 
+    <label className="label" for="name">
+        Enter name:
+        </label><input id="name" type="text" />
+        <button style={{ backgroundColor: 'blue', color: 'white' }}> 
+        { getButtonText() }
+        </button>
+  </div>
+      )
 };
 
 ReactDOM.render(
