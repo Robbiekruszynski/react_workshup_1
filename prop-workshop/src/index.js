3d04b888aaa1574c6ebf7a3c44ac1f4b68ec8b20
robbie <robbiekruszynski@gmail.com>
2019-10-18 16:54:28 -0700

    add bones to post

--- prop-workshop/src/index.js
+++ prop-workshop/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App =() => {
-    return (
-    <div>
-        Hi there!
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Robbie
+          </a>
+          <div className="metadata">
+            <span className="data">Today at 6:00P.M.</span>
+          </div>
+          <div className="text"> Nice post!</div>
+        </div>
+      </div>
     </div>
-    )
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
