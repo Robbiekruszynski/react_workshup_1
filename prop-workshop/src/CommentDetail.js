0fdfa7768c1567e2cc2ddacdef8dba2f87649235
robbie <robbiekruszynski@gmail.com>
2019-10-19 16:06:13 -0700

    add more props

--- prop-workshop/src/CommentDetail.js
+++ prop-workshop/src/CommentDetail.js
@@ -3,21 +3,19 @@ import ReactDOM from "react-dom";
 import faker from "faker";
 
 const CommentDetail = props => {
-  console.log(props);
-
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.authorAvatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="data">Today at 6:00P.M.</span>
+          <span className="data">{props.timeAgo}</span>
         </div>
-        <div className="text"> Nice post!</div>
+        <div className="text"> {props.commentMark}</div>
       </div>
     </div>
   );
--- prop-workshop/src/index.js
+++ prop-workshop/src/index.js
@@ -5,9 +5,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Kate" />
+      <CommentDetail
+        authorAvatar={faker.image.avatar()}
+        author="Sam"
+        commentMark="Hi"
+        timeAgo="Today at 2:45pm"
+      />
+      <CommentDetail
+        authorAvatar={faker.image.avatar()}
+        author="Alex"
+        commentMark="TMI, nobody cares Sam"
+        timeAgo="Today at 5:15pm"
+      />
+      <CommentDetail
+        authorAvatar={faker.image.avatar()}
+        author="Kate"
+        commentMark="Hey, be nice Alex"
+        timeAgo="Today at 6:30pm"
+      />
     </div>
   );
 };
