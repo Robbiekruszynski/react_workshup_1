03076fe762975bd1e1b85ef474c609af9b0bbbd0
robbie <robbiekruszynski@gmail.com>
2019-10-18 22:51:20 -0700

    render multiple comps

--- prop-workshop/src/index.js
+++ prop-workshop/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div className="ui container comments">
       <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
