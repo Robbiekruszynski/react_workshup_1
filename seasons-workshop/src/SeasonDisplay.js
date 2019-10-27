0e3fbb9621b5eff411d43494c76c6a6665496f25
robbie <robbiekruszynski@gmail.com>
2019-10-27 14:10:25 -0700

    add icon work for seasons

--- seasons-workshop/src/SeasonDisplay.js
+++ seasons-workshop/src/SeasonDisplay.js
@@ -14,12 +14,16 @@ const SeasonDisplay = props => {
     season === "winter"
       ? "Kate is probably cold right now"
       : "Robbie is probably sweating";
+  const icon = season === "winter" ? "snowflake" : "sun";
   return (
     <div>
-      {" "}
+      <i className={`${icon} icon`} /> 
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
 
 export default SeasonDisplay;
+
+//go over lines 20 and 22 
