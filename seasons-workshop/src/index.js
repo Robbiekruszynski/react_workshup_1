8032b05792f29cf6920a551b69360337cd62762c
robbie <robbiekruszynski@gmail.com>
2020-02-29 20:42:36 -0800

    update css

--- seasons-workshop/src/index.js
+++ seasons-workshop/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+
+
+
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
 
